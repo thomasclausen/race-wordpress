020b09c22bb4203b93ffcf5ff7bee8d393a123fa
Thomas Clausen <kontakt@thomasclausen.dk>
2013-12-11 10:40:14 +0100

    Update analytics code to Universal Analytics

--- footer.php
+++ footer.php
@@ -1,35 +1,12 @@
 
-	<script type="text/javascript">
-	var myWidth = 0, myHeight = 0;
-	if (typeof(window.innerWidth) == 'number') {
-		//Non-IE
-		myWidth = window.innerWidth;
-		myHeight = window.innerHeight;
-	} else if (document.documentElement && (document.documentElement.clientWidth || document.documentElement.clientHeight)) {
-		//IE 6+ in 'standards compliant mode'
-		myWidth = document.documentElement.clientWidth;
-		myHeight = document.documentElement.clientHeight;
-	} else if (document.body && (document.body.clientWidth || document.body.clientHeight)) {
-		//IE 4 compatible
-		myWidth = document.body.clientWidth;
-		myHeight = document.body.clientHeight;
-	}
+	<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 	
-	var _gaq = _gaq || [];
-	var pluginUrl = '//www.google-analytics.com/plugins/ga/inpage_linkid.js';
-	_gaq.push(['_require', 'inpage_linkid', pluginUrl]);
-	_gaq.push(['_setAccount', 'UA-38361261-2']);
-	_gaq.push(['_trackPageview']);
-	// Viewport data
-	_gaq.push(['_trackEvent', 'Viewport', 'Size', myWidth+'x'+myHeight, true]);
-	_gaq.push(['_trackEvent', 'Viewport', 'Width', myWidth+'x'+myHeight, myWidth, true]);
-	_gaq.push(['_trackEvent', 'Viewport', 'Height', myWidth+'x'+myHeight, myHeight, true]);
-	
-	(function() {
-		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	})();
+	ga('create', 'UA-XXXXXXX-X', 'domain.com');
+	ga('send', 'pageview');
 	</script>
 
 	<?php wp_footer(); ?>
